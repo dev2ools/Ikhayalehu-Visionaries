4c2b030fa789345ecec7dd0bdac2f838ee24db45
dev2ools <asiphendimlana40@gmail.com>
2025-04-05 21:38:41 +0200

    updated home page and added images

--- ikhayalethu-visionaries/src/app/page.js
+++ ikhayalethu-visionaries/src/app/page.js
@@ -6,9 +6,9 @@ export default function Home() {
     <div className="relative min-h-screen">
       {/* Background Image */}
       <div
-        className="absolute inset-0 bg-cover bg-center bg-no-repeat z-10"
+        className="absolute inset-0 bg-cover bg-center bg-no-repeat z-0"
         style={{
-          backgroundImage: "url('https://i.postimg.cc/W1tF9j23/Whats-App-Image-2025-04-03-at-21-16-19-f20db238.jpg')",
+          backgroundImage: "url('https://i.postimg.cc/sfK3ngVR/home2.jpg')",
           backgroundSize: "cover", // Ensures the image covers the entire area
           backgroundPosition: "center", // Centers the image
         }}
@@ -17,25 +17,15 @@ export default function Home() {
       {/* Content */}
       <div className="relative z-20 grid grid-rows-[auto_1fr_auto] items-center justify-items-center min-h-screen text-white">
         <Navbar />
-        <main className="flex flex-col items-center text-center px-4 sm:px-8">
-          <h1 className="text-4xl sm:text-6xl font-bold mb-8">
-            THE FUTURE IS IN OUR HANDS
+        <main className="flex flex-col items-center text-center px-4 sm:px-8 mt-[-95px]">
+          <h1 className="text-4xl sm:text-6xl font-bold mb-8 text-white">
+            <span className="text-blue-600 font-bold">IKHAYALETHU</span> VISIONARIES
           </h1>
-          <section className="text-lg sm:text-xl space-y-6 max-w-3xl">
+          <section className="text-lg sm:text-xl space-y-6 max-w-3xl text-white font-bold">
             <div>
-              <h2 className="text-2xl font-semibold mb-2">Vision</h2>
-              <p>
-                A world where young people have opportunities to create sustainable
-                livelihoods.
-              </p>
-            </div>
-            <div>
-              <h2 className="text-2xl font-semibold mb-2">Mission</h2>
-              <p>
-                We use cutting-edge educational approaches to create pathways for
-                young people from under-resourced communities to pull themselves
-                and each other into sustainable livelihoods.
-              </p>
+              <h2 className="text-2xl font-bold">
+                TOGETHER WE MAKE A <span className="text-blue-600 font-bold">DIFFERENCE</span>
+              </h2>
             </div>
           </section>
         </main>
